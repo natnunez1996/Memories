e3b76c42554540d66e14ed1f428ce82f0698e9b9
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2022-05-06 16:01:41 -0400

    Add files via upload

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,83 @@
+import { AppBar, Avatar, Button, Toolbar, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import memories from "../../images/memoriesLogo.png";
+import memoriesText from "../../images/memoriesText.png";
+import "./navbar.css";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import decode from "jwt-decode";
+
+function Navbar() {
+  const [user, setUser] = useState(JSON.parse(localStorage.getItem("profile")));
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const token = user?.token;
+
+  const logout = () => {
+    dispatch({ type: "LOGOUT" });
+
+    navigate("/", { replace: true });
+  };
+
+  useEffect(() => {
+    //JWT
+    if (token) {
+      const decodedToken = decode(token);
+
+      if (decodedToken.exp * 1000 < new Date().getTime()) logout();
+    }
+    setUser(JSON.parse(localStorage.getItem("profile")));
+  }, [location, token]);
+
+  return (
+    <AppBar
+      style={{ flexDirection: "row" }}
+      className="appBar"
+      position="static"
+      color="inherit"
+    >
+      <div className="brandContainer">
+        <Link to="/">
+          <img src={memoriesText} alt="icon" height="45px" />
+          <img src={memories} alt="memories" height="40px" />
+        </Link>
+      </div>
+      <Toolbar className="toolbar">
+        {user ? (
+          <div className="profile">
+            <Avatar
+              className="purple"
+              alt={user.result.name}
+              src={user.result.imageUrl}
+            >
+              {user.result.name.charAt(0)}
+            </Avatar>
+            <Typography className="userName" variant="h6">
+              {user.result.name}
+            </Typography>
+            <Button
+              variant="contained"
+              className="logout"
+              color="secondary"
+              onClick={logout}
+            >
+              Log Out
+            </Button>
+          </div>
+        ) : (
+          <Button
+            component={Link}
+            to="/auth"
+            variant="contained"
+            color="primary"
+          >
+            Sign In
+          </Button>
+        )}
+      </Toolbar>
+    </AppBar>
+  );
+}
+
+export default Navbar;
