717a784d60289c8bd380e295d9384974547f0b9c
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2022-05-06 15:55:21 -0400

    Add files via upload

---
+++ client/src/actions/posts.js
@@ -0,0 +1,104 @@
+import * as api from '../api'
+import * as actionType from '../constants/actionTypes'
+
+//Action creators 
+
+export const getPosts = (page) => async (dispatch) => {
+    try {
+        dispatch({ type: actionType.START_LOADING })
+        const { data } = await api.fetchPosts(page)
+
+        dispatch({ type: actionType.FETCH_ALL, payload: data })
+        dispatch({ type: actionType.END_LOADING })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const getPost = (id) => async (dispatch) => {
+    try {
+        dispatch({ type: actionType.START_LOADING })
+        const { data } = await api.fetchPost(id)
+
+        dispatch({ type: actionType.FETCH_POST, payload: data })
+        dispatch({ type: actionType.END_LOADING })
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const getPostsBySearch = (searchQuery) => async (dispatch) => {
+    try {
+        dispatch({ type: actionType.START_LOADING })
+        const { data: { data } } = await api.fetchPostsBySearch(searchQuery)
+        console.log(data)
+
+        dispatch({ type: actionType.FETCH_BY_SEARCH, payload: data })
+        dispatch({ type: actionType.END_LOADING })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const createPost = (post, navigate) => async (dispatch) => {
+    try {
+        dispatch({ type: actionType.START_LOADING })
+
+        const { data } = await api.createPost(post);
+
+        navigate(`/posts/${data._id}`, { replace: true })
+        dispatch({ type: actionType.CREATE, payload: data })
+        dispatch({ type: actionType.END_LOADING })
+
+    } catch (error) {
+        console.log(error.message)
+    }
+}
+
+export const deletePost = (id) => async (dispatch) => {
+
+    try {
+        dispatch({ type: actionType.START_LOADING })
+        await api.deletePost(id)
+
+        dispatch({ type: actionType.DELETE, payload: id })
+        dispatch({ type: actionType.END_LOADING })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const updatePost = (id, post) => async (dispatch) => {
+    try {
+
+        const { data } = await api.updatePost(id, post);
+        dispatch({ type: actionType.UPDATE, payload: data })
+
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const likePost = (id) => async (dispatch) => {
+    try {
+        const { data } = await api.likePost(id);
+
+        dispatch({ type: actionType.LIKE, payload: data })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+export const commentPost = (value, id) => async (dispatch) => {
+    try {
+        const { data } = await api.comment(value, id)
+
+        console.log(data)
+
+        dispatch({ type: actionType.COMMENT, payload: data })
+
+        return data.comments
+    } catch (error) {
+        console.log(error)
+    }
+}
