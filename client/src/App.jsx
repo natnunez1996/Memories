93d31ddab90a5e1bc56b54ba4549f6aa10c27f7a
Nathaniel Nunez <76456466+Amznpaks20@users.noreply.github.com>
2022-05-06 16:08:49 -0400

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Container } from "@mui/material";
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Auth from "./components/Auth/Auth";
+import PostDetails from "./components/PostDetails/PostDetails";
+
+function App() {
+  const user = JSON.parse(localStorage.getItem("profile"));
+  console.log(user);
+  return (
+    <BrowserRouter>
+      <Container className="container" maxWidth="xl">
+        <Navbar />
+        <Routes>
+          <Route path="/" exact element={<Navigate to="/posts" replace />} />
+          <Route path="/posts" exact element={<Home />} />
+          <Route path="/posts/search" exact element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route
+            path="/auth"
+            exact
+            element={!user ? <Auth /> : <Navigate to="/posts" replace />}
+          />
+        </Routes>
+      </Container>
+    </BrowserRouter>
+  );
+}
+
+export default App;
